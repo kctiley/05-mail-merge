64aa3c86f4a14861d01db0ae458a8270af05502e
Klint Tiley <kctiley@gmail.com>
2015-09-05 12:43:52 -0600

    Contact list object working and connected to jade page.

--- mail-merge/routes/index.js
+++ mail-merge/routes/index.js
@@ -11,16 +11,30 @@ router.get('/other', function(req, res, next) {
 });
 
 router.post('/other', function (req, res) {
+    var update = false;
     var submit_to = req.body.submit_to;
     var submit_subject = req.body.submit_subject;
     var submit_body = req.body.submit_body;
-    var update = false;
-    var toList =[];
-    toList = req.body.submit_to.split('\n');
+
+    var eachLine =[];
+    eachLine = req.body.submit_to.split('\n');//This is an array of strings        
+    
+    var convertToObject = function (){
+        var objArr = [];
+          for (var i = 0; i < eachLine.length; i++){
+            var obj = {};
+              obj['firstName'] = eachLine[i].split(', ')[0];
+              obj['lastName'] = eachLine[i].split(', ')[1];
+              obj['email'] = eachLine[i].split(', ')[2];
+              objArr.push(obj)
+         }
+         return objArr
+    }
+    var contactInfo = convertToObject();
 
 
     res.render('other', { title: 'Mail Merger Confirmation Page', to: submit_to, subject: submit_subject,
-      body: submit_body, update: true, to_list: toList} )
+      body: submit_body, update: true, to_list: contactInfo} )
 });
 
 module.exports = router;
